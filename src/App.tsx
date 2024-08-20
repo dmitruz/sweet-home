dbbab56c4dcfe00ebcea7664ad0fb67d77766c12
dmitruz <dmitruz970@gmail.com>
2024-08-20 22:41:31 +0300

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Apartments } from './components/Apartments/Apartments';
+import { Header } from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Apartments />
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,27 @@
+
 import styles from './Header.module.css';
+import sweetHome from '../../images/logo.png'
 
 export const Header = () => {
     return (
         <header className={styles.header}>
-            Logo
+            <img className={styles.logo} alt="logo-image" src={sweetHome} />
+            <h2 className={styles.siteName}>Sweet Home</h2>
             <nav className={styles.nav}>
                 <ul className={styles.headerList}>
                     <li className={styles.headerItem}>
                         <a href="/" className={styles.headerLink}>To rent</a>
+                    </li>
+                    <li className={styles.headerItem}>
+                        <a href="/" className={styles.headerLink}>For sale</a>
+                    </li>
+                    <li className={styles.headerItem}>
+                        <a href="/" className={styles.headerLink}>About Us</a>
                     </li>
                 </ul>
             </nav>
         </header>
     );
 }
+
+export default Header;
